5482c6ab402af5828e97ea4e83d190602c9e2ac3
Mohamed <contactdiabysn@gmail.com>
2025-03-01 21:07:22 +0100

    fx

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ require_once SRC . "utils/Base.php";
 session_start();
 
 // constants
-// define("SITE", "https://mohsd-dehors.free.nf");
+// define("SITE", "https://mohsd-dehors.free.nf/");
 define("SITE", "http://localhost:8080/");
 define("STORAGE", SRC . "utils/storage/");
 define("CMPS", SRC . "components/");
@@ -16,12 +16,8 @@ define("IMGS", SITE . "assets/imgs/");
 define("DESC", "");
 
 // funcs 
-$servFn = new Func\ServerFn();
 $fileFn = new Func\FileFn();
-$dateFn = new Func\DateFn();
 $strFn = new Func\StringFn();
-$domFn = new Func\DomFn();
-$envDatas = $fileFn->getEnv();
 
 // APP 
 $title = "Dehors !";
