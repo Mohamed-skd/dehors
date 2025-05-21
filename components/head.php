f739086539ac1f0617d7ee3271aa01c2fb3bfd77
Mohamed <contactdiabysn@gmail.com>
2025-05-21 18:24:26 +0200

    fx

--- components/head.php
+++ components/head.php
@@ -1,9 +1,14 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="<?= DESC ?>" />
+<meta property="og:title" content="<?= $title ?>">
+<meta property="og:description" content="<?= DESC ?>">
+<meta property="og:type" content="website">
+<meta property="og:url" content="<?= SITE ?>">
+<meta property="og:image" content="<?= IMGS . "vignette.jpg" ?>">
 <base target="_blank" />
-<link rel="shortcut icon" href="<?= IMGS . "icon.png" ?>" type="image/png" />
 <link rel="canonical" href="<?= SITE ?>" />
+<link rel="shortcut icon" href="<?= IMGS . "icon.png" ?>" type="image/png" />
 <link rel="stylesheet" href="<?= ASSETS . "style.css"  ?>" />
 <script type="module" src="<?= ASSETS . "script.js" ?>"></script>
 <title><?= $title ?></title>
